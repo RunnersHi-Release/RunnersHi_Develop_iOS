1332ce2e71559956f4b4cfdedb0dd7042fb5fc2e
xwoud <xwoud@naver.com>
2020-09-23 16:26:46 +0900

    [FIX] splash View Controller 구조 변경

--- Runners_Hi/Runners_Hi/Source/ViewController/SplashVC.swift
+++ Runners_Hi/Runners_Hi/Source/ViewController/SplashVC.swift
@@ -14,21 +14,27 @@ class SplashVC: UIViewController {
    var animationView: AnimationView?
 
    override func viewDidLoad() {
-       super.viewDidLoad()
+    super.viewDidLoad()
+    setView()
     print(UIDevice.current.identifierForVendor?.uuidString)
-       animationView = AnimationView(name: "splash")
-    animationView?.contentMode = .scaleAspectFit
-       animationView?.frame = self.view.bounds
-       animationView?.play()
-       // Do any additional setup after loading the view.
-       self.view.addSubview(animationView!)
-       Timer.scheduledTimer(timeInterval: 3, target: self, selector: #selector(aaaa), userInfo: nil, repeats: false)
    }
    
-   
-   @objc func aaaa() {
-       guard let storyboard = UIStoryboard(name: "Tab", bundle: nil).instantiateViewController(identifier: "TabBarController") as? UITabBarController else { return }
-       UIApplication.shared.windows.filter {$0.isKeyWindow}.first?.rootViewController = storyboard
-   }
 
+}
+extension SplashVC {
+    func setView() {
+        animationView = AnimationView(name: "splash")
+        animationView?.contentMode = .scaleAspectFit
+           animationView?.frame = self.view.bounds
+           animationView?.play()
+           // Do any additional setup after loading the view.
+           self.view.addSubview(animationView!)
+           Timer.scheduledTimer(timeInterval: 3, target: self, selector: #selector(aaaa), userInfo: nil, repeats: false)
+    }
+    
+    @objc func aaaa() {
+        guard let storyboard = UIStoryboard(name: "Tab", bundle: nil).instantiateViewController(identifier: "TabBarController") as? UITabBarController else { return }
+        UIApplication.shared.windows.filter {$0.isKeyWindow}.first?.rootViewController = storyboard
+    }
+    
 }
