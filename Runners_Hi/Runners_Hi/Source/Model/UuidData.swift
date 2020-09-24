2220ba7385808f80e6c3bad88dde31d1a72e27fc
xwoud <xwoud@naver.com>
2020-09-24 23:24:42 +0900

    [UPDATE] UUID DataModel 생성

---
+++ Runners_Hi/Runners_Hi/Source/Model/UuidData.swift
@@ -0,0 +1,23 @@
+//
+//  UuidData.swift
+//  Runners_Hi
+//
+//  Created by 김민희 on 2020/09/24.
+//  Copyright © 2020 김민희. All rights reserved.
+//
+
+import Foundation
+
+struct UuidData: Codable {
+    let status: Int
+    let success: Bool
+    let message: String
+    let data: UuidDetail
+}
+
+struct UuidDetail: Codable {
+    let accessToken, nickname: String
+    let gender, image: Int
+    let badge: String
+    let win, lose: Int
+}
