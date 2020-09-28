fbc22fc3dfdacc5e7d749af04cde3a8abb721d19
최영재 <brandon94_@naver.com>
2020-09-28 18:00:48 +0900

    [UPDATE] 기록 보기 뷰 오토레이아웃 완성

---
+++ Runners_Hi/Runners_Hi/Source/ViewController/RecordDetailVC.swift
@@ -0,0 +1,25 @@
+//
+//  RecordDetailVC.swift
+//  Runners_Hi
+//
+//  Created by 최영재 on 2020/09/28.
+//  Copyright © 2020 김민희. All rights reserved.
+//
+
+import UIKit
+
+class RecordDetailVC: UIViewController {
+    @IBOutlet weak var recordDetailBackButton: UIButton!
+    @IBOutlet weak var recordDateLabel: UILabel!
+    @IBOutlet weak var recordTimeLabel: UILabel!
+    @IBOutlet weak var recordNaverView: UIView!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
