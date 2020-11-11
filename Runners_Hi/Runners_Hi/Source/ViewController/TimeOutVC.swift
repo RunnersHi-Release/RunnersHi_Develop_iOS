e54a62a4807c02e70afad33dd9f729c9c5bedc36
xwoud <xwoud@naver.com>
2020-11-11 17:42:39 +0900

    [UPDATE] TimeOutVC - 레이아웃 속성 추가

--- Runners_Hi/Runners_Hi/Source/ViewController/TimeOutVC.swift
+++ Runners_Hi/Runners_Hi/Source/ViewController/TimeOutVC.swift
@@ -10,21 +10,44 @@ import UIKit
 
 class TimeOutVC: UIViewController {
 
+    @IBOutlet weak var topLayout: NSLayoutConstraint!
+    @IBOutlet weak var firtstMentLabel: UILabel!
+    @IBOutlet weak var secondMentLabel: UILabel!
+    @IBOutlet weak var nextButton: UIButton!
+    @IBOutlet weak var goodButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        setView()
         // Do any additional setup after loading the view.
     }
     
 
-    /*
-    // MARK: - Navigation
+}
+extension TimeOutVC {
+    func setView() {
+        topLayout.constant = (208.0/623.0) * self.view.frame.height
+        print(topLayout.constant)
+        firtstMentLabel.text = "아쉽게도 지금은\n러너를 찾을 수 없네요"
+        firtstMentLabel.numberOfLines = 0
+        firtstMentLabel.font = UIFont(name: "NanumSquareB", size: 20)
+        
+        secondMentLabel.text = "대신 나의 기록과 러닝하시겠어요?\n어제의 나를 이겨보세요"
+        secondMentLabel.numberOfLines = 0
+        secondMentLabel.font = UIFont(name: "NanumSquareR", size: 16)
+        
+        nextButton.backgroundColor = UIColor.brownishGrey
+        nextButton.titleLabel?.font = UIFont(name: "NanumSquareB", size: 16)
+        nextButton.setTitle("다음에 할래요", for: .normal)
+        nextButton.setTitleColor(.white, for: .normal)
+        nextButton.layer.cornerRadius = 8
+        
+        goodButton.backgroundColor = UIColor.lightishBlue
+        goodButton.titleLabel?.font = UIFont(name: "NanumSquareB", size: 16)
+        goodButton.setTitle("좋아요", for: .normal)
+        goodButton.setTitleColor(.white, for: .normal)
+        goodButton.layer.cornerRadius = 8
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+
     }
-    */
-
 }
