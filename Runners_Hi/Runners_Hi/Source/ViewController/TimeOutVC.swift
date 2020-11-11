01680d498bd745ccead4621d252f921d1581e5e1
xwoud <xwoud@naver.com>
2020-11-11 16:56:37 +0900

    [UPDATE] 매칭 TimeOut시 VC AutoLayout 완성

---
+++ Runners_Hi/Runners_Hi/Source/ViewController/TimeOutVC.swift
@@ -0,0 +1,30 @@
+//
+//  TimeOutVC.swift
+//  Runners_Hi
+//
+//  Created by 김민희 on 2020/11/11.
+//  Copyright © 2020 김민희. All rights reserved.
+//
+
+import UIKit
+
+class TimeOutVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
