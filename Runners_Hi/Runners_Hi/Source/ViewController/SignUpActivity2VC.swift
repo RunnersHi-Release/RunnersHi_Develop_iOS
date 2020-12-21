0b6ff367ae317a97df0e44bc51a97216507c4cd9
최영재 <brandon94_@naver.com>
2020-12-21 22:18:45 +0900

    [UPDATE] 회원가입 중간 커밋

---
+++ Runners_Hi/Runners_Hi/Source/ViewController/SignUpActivity2VC.swift
@@ -0,0 +1,30 @@
+//
+//  SignUpActivity2VC.swift
+//  Runners_Hi
+//
+//  Created by 최영재 on 2020/12/21.
+//  Copyright © 2020 김민희. All rights reserved.
+//
+
+import UIKit
+
+class SignUpActivity2VC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
